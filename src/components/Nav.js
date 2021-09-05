faa370550669bb056a6413f0eda06f3972c2b385
artofmayhem <chef_tony_miller@yahoo.com>
2021-09-04 22:34:59 -1000

    nav finished

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,19 +4,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import Button from "@material-ui/core/Button";
 import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import MenuIcon from "@material-ui/icons/Menu";
-// import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
-// import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
-// import LocalBarIcon from "@material-ui/icons/LocalBar";
-// import LocalDiningIcon from "@material-ui/icons/LocalDining";
-// import LocalGroceryStoreIcon from "@material-ui/icons/LocalGroceryStore";
-// import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-// import FacebookIcon from "@material-ui/icons/Facebook";
-// import InstagramIcon from "@material-ui/icons/Instagram";
-// import TwitterIcon from "@material-ui/icons/Twitter";
+import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import LocalBarIcon from "@material-ui/icons/LocalBar";
+import LocalDiningIcon from "@material-ui/icons/LocalDining";
+import LocalGroceryStoreIcon from "@material-ui/icons/LocalGroceryStore";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import React, { useState, useEffect, useRef } from "react";
 import { TimelineLite } from "gsap";
 import { Link } from "react-router-dom";
@@ -24,11 +24,16 @@ import { Link } from "react-router-dom";
 const useStyles = makeStyles({
   list: {
     width: 250,
+    color: 'black'
   },
   fullList: {
     width: "50vw",
-    backgroundColor: 'black'
+    backgroundColor: "black",
   },
+  listItemText: {
+    color: 'white',
+    fontSize: '1.5rem'
+  }
 });
 
 export default function Nav() {
@@ -72,7 +77,9 @@ export default function Nav() {
         ref={(el) => (nav = el)}
       >
         <div
-          className={"flex flex-row flex-wrap justify-between align-center w-screen"}
+          className={
+            "flex flex-row flex-wrap justify-between align-center w-screen"
+          }
           style={{ backgroundColor: "transparent" }}
         >
           <div
@@ -86,7 +93,13 @@ export default function Nav() {
             </Button>
             <Link to="/">
               <h1 className={"ml-12 text-gray-400"}>SONVR Design</h1>
-              <h2 className={'text-gray-400 ml-80 pl-7 -mt-11 text-lg font-medium'}>By Tony Miller</h2>
+              <h2
+                className={
+                  "text-gray-400 ml-80 pl-7 -mt-11 text-lg font-medium"
+                }
+              >
+                By Tony Miller
+              </h2>
             </Link>
           </div>
           {/* <div className={"my-6 -mr-10 text-black text-4xl w-96"}>
@@ -133,6 +146,7 @@ export default function Nav() {
         </div>
       </AppBar>
       <Drawer
+        anchor="left"
         color="primary"
         open={state.top}
         onClose={handleDrawerClose}
@@ -140,48 +154,51 @@ export default function Nav() {
           paper: classes.fullList,
         }}
       >
-        <List className={classes.list}>
-          {/* <ListItem button>
-            <ListItemIcon>
-              <EmojiPeopleIcon />
-            </ListItemIcon>
-            <Link to={"about"} onClick={handleDrawerClose}>
-              <ListItemText primary="About" />
+       <List className={classes.list}>
+       <ListItem >
+              <h1 className={classes.listItemText} >Projects</h1>
+          </ListItem>
+          <hr style={{color: '#444', width: '30vw', minWidth: 375}}/>
+          <ListItem button>
+            <a href={"https://grubspace.netlify.app"} className={'mt-6'} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}} >Grubspace</h3>
+            </a>
+          </ListItem>
+          <ListItem button>
+            <Link to={"https://apple-clone-site.netlify.app"} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}}>Apple Clone</h3>
             </Link>
           </ListItem>
           <ListItem button>
-            <ListItemIcon>
-              <LibraryBooksIcon />
-            </ListItemIcon>
-            <Link to={"chef"} onClick={handleDrawerClose}>
-              <ListItemText primary="The Chef Spot" />
-            </Link>
+            <a href={"https://anywhere-fitness-club.vercel.app"} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}} >Anywhere Fitness Club</h3>
+            </a>
           </ListItem>
           <ListItem button>
-            <ListItemIcon>
-              <LocalDiningIcon />
-            </ListItemIcon>
-            <Link to={"recipes"} onClick={handleDrawerClose}>
-              <ListItemText primary="Recipe Box" />
-            </Link>
+        
+            <a href={'https://streetartmagazine.netlify.app'} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}}>Street Art Magazine</h3>
+            </a>
           </ListItem>
           <ListItem button>
-            <ListItemIcon>
-              <LocalGroceryStoreIcon />
-            </ListItemIcon>
-            <Link to={"nutrition"} onClick={handleDrawerClose}>
-              <ListItemText primary="Nutrition Corner" />
-            </Link>
+       
+          <a href={'https://adidasproductportal.vercel.app'} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}}>Adidas Product Portal</h3>
+            </a>
           </ListItem>
           <ListItem button>
-            <ListItemIcon>
-              <LocalBarIcon />
-            </ListItemIcon>
-            <Link to={"cocktails"} onClick={handleDrawerClose}>
-              <ListItemText primary="Wine Source" />
-            </Link>
-          </ListItem> */}
+          <a href={'https://syntech3d.netlify.app'} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}}>Syntech 3D Header</h3>
+            </a>
+          </ListItem>
+          <ListItem button>
+          <a href={'https://drive.google.com/file/d/17TZBey8iB3NjTFbU3Sc96eVprXbRWdB6/view?usp=sharing'} className={'my-32'} onClick={handleDrawerClose}>
+              <h3 style={{color: 'white'}}>Technical Resume - Tony Miller</h3>
+            </a>
+          </ListItem>
         </List>
+
+        
       </Drawer>
     </>
   );
